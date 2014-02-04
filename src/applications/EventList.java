a1c01d9ec2fbde09d1ad37b10440458017438617
alexemmons <alex.emmons@gmail.com>
2014-02-03 21:27:46 -0600

    Encapsulated finishTime and cleaned up EventList a bit (more to do).

--- src/applications/EventList.java
+++ src/applications/EventList.java
@@ -2,22 +2,21 @@ package applications;
 
 public class EventList {
     // data members
-    int[] finishTime; // finish time array
+    private int[] finishTime; // finish time array
 
     // constructor
-    EventList(int theNumMachines, int theLargeTime) {// initialize
-                                                             // finish
-                                                             // times for
-                                                             // m
-                                                             // machines
-        if (theNumMachines < 1)
+    public EventList(int theNumMachines, int theLargeTime) {
+        // initialize finish times for machines
+        if (theNumMachines < 1) {
             throw new IllegalArgumentException(MachineShopSimulator.NUMBER_OF_MACHINES_MUST_BE_AT_LEAST_1);
+        }
         finishTime = new int[theNumMachines + 1];
 
         // all machines are idle, initialize with
         // large finish time
-        for (int i = 1; i <= theNumMachines; i++)
-            finishTime[i] = theLargeTime;
+        for (int i = 1; i <= theNumMachines; i++) {
+            getFinishTime()[i] = theLargeTime;
+        }
     }
 
     /** @return machine for next event */
@@ -25,20 +24,24 @@ public class EventList {
         // find first machine to finish, this is the
         // machine with smallest finish time
         int p = 1;
-        int t = finishTime[1];
-        for (int i = 2; i < finishTime.length; i++)
-            if (finishTime[i] < t) {// i finishes earlier
+        int t = getFinishTime()[1];
+        for (int i = 2; i < getFinishTime().length; i++)
+            if (getFinishTime()[i] < t) {// i finishes earlier
                 p = i;
-                t = finishTime[i];
+                t = getFinishTime()[i];
             }
         return p;
     }
 
-    int nextEventTime(int theMachine) {
-        return finishTime[theMachine];
+    public int nextEventTime(int theMachine) {
+        return getFinishTime()[theMachine];
     }
 
-    void setFinishTime(int theMachine, int theTime) {
+    public void setFinishTime(int theMachine, int theTime) {
         finishTime[theMachine] = theTime;
+    }
+
+    public int[] getFinishTime() {
+        return finishTime;
     }
 }
