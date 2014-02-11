20a935accb1f263042237e3fca9d4c5dc8220907
alexemmons <alex.emmons@gmail.com>
2014-02-10 20:46:49 -0600

    Returned inputData() to original version.

--- src/applications/MachineShopSimulator.java
+++ src/applications/MachineShopSimulator.java
@@ -76,11 +76,8 @@ public class MachineShopSimulator {
             if (machines.getMachine(theMachine).getJobQ().isEmpty()) {// no waiting job
                 eList.setFinishTime(theMachine, largeTime);
             } else {// take job off the queue and work on it
-                machines.getMachine(theMachine).setActiveJob((Job) machines.getMachine(theMachine).getJobQ()
-                        .remove());
-                machines.getMachine(theMachine).setTotalWait(machines.getMachine(theMachine).getTotalWait()
-                        + (timeNow
-                                - machines.getMachine(theMachine).getActiveJob().getArrivalTime()));
+                machines.getMachine(theMachine).setActiveJob((Job) machines.getMachine(theMachine).getJobQ().remove());
+                machines.getMachine(theMachine).setTotalWait(machines.getMachine(theMachine).getTotalWait() + (timeNow - machines.getMachine(theMachine).getActiveJob().getArrivalTime()));
                 machines.getMachine(theMachine).setNumTasks(machines.getMachine(theMachine).getNumTasks() + 1);
                 int t = machines.getMachine(theMachine).getActiveJob().removeNextTask();
                 eList.setFinishTime(theMachine, timeNow + t);
@@ -126,10 +123,6 @@ public class MachineShopSimulator {
         }
 
         // input the jobs
-        inputJobs(keyboard);
-    }
-
-    private static void inputJobs(MyInputStream keyboard) {
         Job theJob;
         for (int i = 1; i <= numJobs; i++) {
             System.out.println("Enter number of tasks for job " + i);
@@ -141,25 +134,20 @@ public class MachineShopSimulator {
 
             // create the job
             theJob = new Job(i);
-            firstMachine = createJob(keyboard, theJob, tasks, firstMachine); // task queue
+            System.out.println("Enter the tasks (machine, time)" + " in process order");
+            for (int j = 1; j <= tasks; j++) {// get tasks for job i
+                int theMachine = keyboard.readInteger();
+                int theTaskTime = keyboard.readInteger();
+                if (theMachine < 1 || theMachine > numMachines || theTaskTime < 1) {
+                    throw new MyInputException(BAD_MACHINE_NUMBER_OR_TASK_TIME);
+                }
+                if (j == 1) {
+                    firstMachine = theMachine; // job's first machine
+                }
+                theJob.addTask(theMachine, theTaskTime); // add to
+            }
             machines.getMachine(firstMachine).getJobQ().put(theJob);
         }
-    }
-
-    private static int createJob(MyInputStream keyboard, Job theJob, int tasks, int firstMachine) {
-        System.out.println("Enter the tasks (machine, time)" + " in process order");
-        for (int j = 1; j <= tasks; j++) {// get tasks for job i
-            int theMachine = keyboard.readInteger();
-            int theTaskTime = keyboard.readInteger();
-            if (theMachine < 1 || theMachine > numMachines || theTaskTime < 1) {
-                throw new MyInputException(BAD_MACHINE_NUMBER_OR_TASK_TIME);
-            }
-            if (j == 1) {
-                firstMachine = theMachine; // job's first machine
-            }
-            theJob.addTask(theMachine, theTaskTime); // add to
-        }
-        return firstMachine;
     }
 
     /** load first jobs onto each machine */
